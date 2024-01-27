cb1c28ff2549ccee2b7ba6f9d73307ef5b2d2ab4
Luca Tescari <lucatescari@users.noreply.github.com>
2024-01-27 15:10:25 +0100

    started on speech

--- FunnyCassette/Assets/Scripts/TextPresenter.cs
+++ FunnyCassette/Assets/Scripts/TextPresenter.cs
@@ -5,9 +5,31 @@ using UnityEngine;
 public class TextPresenter : MonoBehaviour
 {
     [SerializeField] TMPro.TMP_Text shownText;
+    [SerializeField] private AudioClip dialogTypingSoundClip;
+    private AudioSource audioSource;
 
+    private void Awake() {
+        audioSource = this.gameObject.AddComponent<AudioSource>();
+    }
 
     public void PresentText(string text) {
-        shownText.text = text;
+        shownText.text = "";
+
+        StartCoroutine(PrintText(text));
+
+        // shownText.text = text;
+    }
+
+    private IEnumerator PrintText(string text) {
+        shownText.maxVisibleCharacters = 0;
+
+        foreach (char letter in text.ToCharArray()) {
+            audioSource.PlayOneShot(dialogTypingSoundClip);
+
+            shownText.text += letter;
+
+            shownText.maxVisibleCharacters++;
+            yield return new WaitForSeconds(0.1f);
+        }
     }
 }
