d74471a439c2b1501c23926104ad06076c048877
Raphael Anderegg <raphael.anderegg@bitforge.ch>
2024-02-08 14:32:53 +0100

    added randomized dialogs to game

--- FunnyCassette/Assets/Scripts/DialogSystem/Dialog.cs
+++ FunnyCassette/Assets/Scripts/DialogSystem/Dialog.cs
@@ -9,9 +9,21 @@ public class Dialog
 {
     public List<DialogPhrase> dialogs;
     
+    // start at dialog_00!
+    private const int dialogsCount = 11;
+
+    private static int CurrentDialogIndex = -1;
+    
     public static Dialog CreateFromJSON()
     {
-        string jsonString = File.ReadAllText(Application.streamingAssetsPath + "/dialog.json");
+        if(CurrentDialogIndex == -1)
+            CurrentDialogIndex = UnityEngine.Random.Range(0, dialogsCount);
+        else
+            CurrentDialogIndex = (CurrentDialogIndex + 1) % dialogsCount;
+        
+        var fileName = "dialog_" + CurrentDialogIndex.ToString("00") + ".json";
+        
+        string jsonString = File.ReadAllText(Application.streamingAssetsPath + "/" + fileName);
         var dialog = JsonUtility.FromJson<Dialog>(jsonString);
         dialog.dialogs = dialog.dialogs.OrderBy(x => Guid.NewGuid()).ToList();
         return dialog;
